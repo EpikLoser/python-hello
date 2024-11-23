69188dac02881f244846f6c50669aad1a7a40881
EpikLoser <caedryn.delaney@tuta.com>
2024-11-23 15:53:20 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+def sum(x, y):
+    return float(x) * float(y)
+
+def main():
+ print ("Enter first number:")
+    x = input()
+    print ("Enter second number:")
+    y = input()
+
+    print("The sum of the numbers is", format(sum(x, y)))
+
+main()
